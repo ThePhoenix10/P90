632de9cd3b543192b93d11e1373676719b135edb
ThePhoenix10 <84483564+ThePhoenix10@users.noreply.github.com>
2022-04-01 16:04:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function login(){
+    player1_input = document.getElementById("player1_input").value;
+    player2_input = document.getElementById("player2_input").value;
+    localStorage.setItem("player1_input", player1_input);
+    localStorage.setItem("player2_input", player2_input);
+    window.location = "game.html";
+}
